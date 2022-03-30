b3953533623392e995c3e96cf97d931d9ef6e0aa
thisvideolab <thisvideolab@gmail.com>
2022-03-31 01:07:03 +0900

    220331 java extra study

--- src/private_work/clonecording.java
+++ src/private_work/clonecording.java
@@ -1,2 +1,19 @@
 package private_work;
 
+class A{
+	int a;
+	 void run() {
+		 int b;
+		 System.out.println(a);
+	}
+}
+
+public class clonecording{
+	
+	public static void main(String[]args) {
+		A a = new A();
+		a.run();
+
+	}
+	
+}
